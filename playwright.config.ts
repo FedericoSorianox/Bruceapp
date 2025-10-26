268e360f1276ccd211004c74da647fd62fd47767
Your Name <your.email@example.com>
2025-10-26 18:14:57 -0300

    Update Playwright configuration to include BASE_URL from secrets and streamline dotenv usage

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import 'dotenv/config';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
+
   timeout: 40000,
-  //fullyParallel: false, // Deshabilitar paralelización para evitar conflictos de autenticación
-  forbidOnly: !!process.env.CI,
+
+  fullyParallel: true,
+
   retries: process.env.CI ? 2 : 0,
-  workers: 1, // Usar solo 1 worker para evitar conflictos de storageState
+
+  workers: undefined,
+
   reporter: 'html',
+
   // Configuration for the browser and device to use for testing
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
